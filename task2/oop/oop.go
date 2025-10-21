2b233050843d56ef5f752a2d91a7f70f95df7a71
fengsc46-ai <fengsc46@gmail.com>
2025-10-21 23:07:26 +0800

    feat:完成作业task2：面向对象

---
+++ task2/oop/oop.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+/*
+*定义一个 Shape 接口，包含 Area() 和 Perimeter() 两个方法。然后创建 Rectangle 和 Circle 结构体，
+实现 Shape 接口。在主函数中，创建这两个结构体的实例，并调用它们的 Area() 和 Perimeter() 方法。
+*/
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (rect *Rectangle) Area() float64 {
+	return rect.width * rect.height
+}
+func (rect *Rectangle) Perimeter() float64 {
+	return 2 * (rect.width + rect.height)
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (r *Circle) Perimeter() float64 {
+	return r.radius * r.radius
+}
+func (c *Circle) Area() float64 {
+	return (22 / 7.0) * (c.radius * c.radius)
+}
+
+func main() {
+	rect := Rectangle{10, 5}
+	circ := Circle{3}
+	fmt.Println("Rectangle area:", rect.Area())
+	fmt.Println("Rectangle perimeter:", rect.Perimeter())
+	fmt.Println("Circle perimeter:", circ.Perimeter())
+	fmt.Println("Circle area:", circ.Area())
+	fmt.Println("=============================================================")
+	emp := Employee{Person{"John", 30}, 1001}
+	emp.PrintInfo()
+}
+
+/*
+*
+使用组合的方式创建一个 Person 结构体，包含 Name 和 Age 字段，再创建一个 Employee 结构体，组合 Person 结构体并添加 EmployeeID 字段。
+为 Employee 结构体实现一个 PrintInfo() 方法，输出员工的信息。
+*/
+type Person struct {
+	Name string
+	Age  int
+}
+
+type Employee struct {
+	Person
+	EmployeeID int
+}
+
+func (e *Employee) PrintInfo() {
+	fmt.Printf("Name: %s, Age: %d, EmployeeID: %d\n", e.Name, e.Age, e.EmployeeID)
+}
